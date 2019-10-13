87c3c05fbb15ffa7e6ad0f508ca417be9f5ff45e
Roman Lynnyk <roman.lynnyk.sa.@gmail.com>
2019-10-13 14:54:05 +0300

    updated homework task

--- Task2_decorator.py
+++ Task2_decorator.py
@@ -5,41 +5,20 @@ string = input()
 def make_GT(function) -> str:
     def escapist():
         func = function()
-        make_replace = func.replace('>', '&gt')
+        make_replace = func.replace('&', '&amp').replace('"', '&quot').replace('<', '&lt').replace('>', '&gt')
         return make_replace
+
     return escapist
 
 
-def make_IT(function) -> str:
-    def escapist():
-        func = function()
-        make_replace = func.replace('<', '&lt')
-        return make_replace
-    return escapist
-
-
-def make_Amp(function) -> str:
-    def escapist():
-        func = function()
-        make_replace = func.replace('&', '&amp')
-        return make_replace
-    return escapist
-
-
-def make_Quot(function) -> str:
-    def escapist():
-        func = function()
-        make_replace = func.replace('"', '&quot')
-        return make_replace
-    return escapist
-
-
-@make_Quot
 @make_GT
-@make_IT
-@make_Amp
 def escape():
     return string
 
 
-print(escape())
+def main():
+    print(escape())
+
+
+if __name__ == '__main__':
+    main()
