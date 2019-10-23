d96daecfb4af4b7a69553e86521f30b50d6afd18
monuelDePari <roman.lynnyk.sa@gmail.com>
2019-10-23 20:39:44 +0300

    fixed task1

--- Task1_Sum_of_List.py
+++ Task1_Sum_of_List.py
@@ -1,12 +1,16 @@
 from pip._vendor.msgpack.fallback import xrange
 
 
+def Printer(some_list, sum_of_list, average):
+    print(some_list, sum_of_list, average)
+
+
 def generate_random():
     import random
     some_list = random.sample(xrange(100), 10)
     sum_of_list = sum(some_list)
     average = sum_of_list / len(some_list)
-    print(some_list, sum_of_list, average)
+    Printer(some_list, sum_of_list, average)
 
 
 def main():
