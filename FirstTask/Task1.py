ae37f5424c82d0e3f8d31562e3b851380e04cd48
monuelDePari <roman.lynnyk.sa@gmail.com>
2019-10-24 19:46:13 +0300

    Fixed Task1

--- FirstTask/Task1.py
+++ FirstTask/Task1.py
@@ -1,16 +1,20 @@
 from pip._vendor.msgpack.fallback import xrange
 
 
-def Printer(some_list, sum_of_list, average):
-    print(some_list, sum_of_list, average)
+def printer(string):
+    print(string)
 
 
 def generate_random():
     import random
+
     some_list = random.sample(xrange(100), 10)
+
     sum_of_list = sum(some_list)
+
     average = sum_of_list / len(some_list)
-    Printer(some_list, sum_of_list, average)
+
+    printer('{} {} {}'.format(some_list, sum_of_list, average))
 
 
 def main():
